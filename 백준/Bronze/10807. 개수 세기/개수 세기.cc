47df2a27b752dc30a51338c8a30a2136d7fb67c2
Hi im new <55071790+gosu1@users.noreply.github.com>
2023-08-17 16:52:35 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int n;
+    int arr[101];
+    int v;
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cin >> v;
+    int res = 0;
+    for (int i =0; i<n; i++){
+        if(arr[i] == v)
+            res++;
+    }
+    cout << res;
+}
