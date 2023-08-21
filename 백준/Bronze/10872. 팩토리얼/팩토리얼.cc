7be5124b2283fb8b49d6f1dc963346715dbbdb3c
Hi im new <55071790+gosu1@users.noreply.github.com>
2023-08-21 15:23:06 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n ;
+    cin >> n;
+    int result = 1;
+
+    if (n==0){
+        cout <<1;
+    } else {
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        cout << result;
+    }
+
+
+    return 0;
+}
